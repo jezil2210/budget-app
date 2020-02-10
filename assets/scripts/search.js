ed37c229b11be77edf6c0745fc5655b80060fa4f
jezil2210 <jezil2210@yahoo.com>
2020-02-10 18:40:27 -0300

    updating folders

--- assets/scripts/search.js
+++ assets/scripts/search.js
@@ -5,13 +5,15 @@ let modal = document.querySelector("#modalRegistraDespesa")
 let row = document.querySelector('#changeClass')
 let i = document.querySelector('#i')
 
-//SEARCH
+//REGISTER
 let modalFather = $('#modalFather');
 
 buttonRegister.addEventListener('click', () => {
   registerBudget()
 })
 
+//SEARCH
+let allowed = true
 search.addEventListener('click', () => {
 
   span.innerHTML = "Search a new Budget"
@@ -19,8 +21,11 @@ search.addEventListener('click', () => {
   buttonRegister.addEventListener('click', () => {
     searchBudget()
   })
-
-  modalFather.append("<div class='row'><div class='col'><table class='table' ><thead><tr><th>Date</th><th>Type</th><th>Description</th><th>Value</th><th></th></tr></thead><tbody id='budgetList'> </tbody></table></div></div>")
+  
+  if(allowed){
+    modalFather.append("<div class='row' style='margin-left: 8%; margin-top: 2%;'><div class='col'><table class='table' ><thead><tr><th>Date</th><th>Type</th><th>Description</th><th>Value</th><th></th></tr></thead><tbody id='budgetList'> </tbody></table></div></div>")
+    allowed = false
+  }
 
   //modalFather.style.marginLeft = '30px'
 
